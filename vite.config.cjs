fee5f4c70e58c03420876a3f4ca87c06c088a4ec
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-07-11 16:28:43 +0900

    ビルドを修正

--- vite.config.cjs
+++ vite.config.cjs
@@ -1,3 +1,17 @@
-export default {
-  base: "/website-test/"
-};
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        eula: resolve(__dirname, 'eula.html'),
+        about: resolve(__dirname, 'about.html'),
+        utau: resolve(__dirname, 'utau.html'),
+        changelog: resolve(__dirname, 'changelog.html'),
+        404: resolve(__dirname, '404.html'),
+      },
+    },
+  },
+  base: "/website-test/",
+})
