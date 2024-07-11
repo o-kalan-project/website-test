9208da0bf0f93529d4c36872924902dd0e63f209
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-07-11 23:47:47 +0900

    発狂

---
+++ src/footer.ts
@@ -0,0 +1,16 @@
+import '../main.css';
+document.body.insertAdjacentHTML('beforeend',`
+<footer class="flex items-center w-screen bg-white z-10 border-t border-gray-300">
+      <nav class="mr-auto py-auto justify-start p-3 md:ml-10">
+        <ul class="flex items-center gap-4 whitespace-nowrap">
+          <li>
+            <a
+              style="color: pink"
+              class="text-[min(3vw,24px)]"
+              >桜花爛漫</a
+            >
+          </li>
+        </ul>
+      </nav>
+      </footer>
+`);
