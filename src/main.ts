e49cac8da179cb1bbb7a0d902aa35a8f9aec1104
creeper-0910 <56744841+creeper-0910@users.noreply.github.com>
2024-07-11 09:02:21 +0900

    遅延追加

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import '../main.css';
 document.querySelector<HTMLDivElement>('#app')!.innerHTML += `
   <div class="flex justify-between items-center gap-4">
-  <div class="animate-slide-in-bck-left text-[min(3vw,34px)]">
+  <div class="animate-slide-in-bck-left text-[min(3vw,34px)] animation-delay-600">
       <h1 class="text-[min(4vw,50px)]">応歌ランとは</h1><BR>
       応歌ラン(おうか らん)とは、2024年6月8日のニコニコ動画サイバー攻撃事件を発端とし、ニコニコ動画を応援することを目的に有志が集まり制作したオリジナルキャラクターです。
     </div>
